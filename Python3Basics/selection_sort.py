75c52d5b0c8e895784c18f9a4e64541e5b621822
supriyo97 <supriyam789@gmail.com>
2019-03-12 02:01:32 +0530

    Selection sort implementation

--- Python3Basics/selection_sort.py
+++ Python3Basics/selection_sort.py
@@ -1,10 +1,10 @@
 def selection_sort(array):                   #SELECTION SORT function
     for i in range((len(array)-1)):          #array is group of elements/same types of values
-        minvalue = i                         #minvalue is the minimum value
+         = i                         #minindex is the minimum index fo the array
         for j in range(i+1,len(array)):
-            if array[j]<array[minvalue]:
-                minvalue = j
-        array[i],array[minvalue] = array[minvalue],array[i]             #exchange the values between intial values and minimum values
+            if array[j]<array[minindex]:
+                minindex = j
+        array[i],array[minindex] = array[minindex],array[i]             #exchange the values between intial values and minimum index
     return a
 
 a = [123,242,12,224,11,5435,212]
